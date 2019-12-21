d95db8b6bdb5d247af4c34aa3ee11ce6f9306a18
Mapachito <58920957+EsMapachito@users.noreply.github.com>
2019-12-21 23:53:34 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+description 'PoliceOfficer_mapachito'
+
+fx_version "adamant"
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_scripts {
+   'mapachito_c.lua',
+   'mapachito_config.lua'
+}
+
+server_scripts {
+'mapachito_s.lua',
+'mapachito_config.lua'
+}
---
+++ mapachito_config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.HuntingMessage = '(E) Enrolarse'
+Config.KillingMessage = 'Ya eres un Agente de la Ley'
+Config.Price = 100
+Config.Xp = 100
